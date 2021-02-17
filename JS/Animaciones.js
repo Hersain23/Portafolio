d3ca455ee049a3290043e545b0a5564ed4debad1
unknown <dguarnerosmarquez@gmail.com>
2021-02-17 01:06:07 -0600

    Portafolio 2.1

--- JS/Animaciones.js
+++ JS/Animaciones.js
@@ -1,46 +1,35 @@
-const $observador = document.querySelectorAll(".observador");
-const $acerca = document.getElementById("acerca"),
-$yo=document.getElementById("yo"),
-$titulo = document.querySelectorAll(".titulo"),
-$contenidoTexo = document.querySelectorAll(".contenido-texto");
-const $habilidades = document.getElementById("habilidades");
-const $empleos = document.getElementById("empleos");
+const $observador = document.querySelectorAll(".ob");
+const $acerca = document.getElementById("acerca");
+const $intereses = document.querySelectorAll(".container-interes-contenido");
 const options = {
     root:document.getElementById("container"),
     threshold:.45
 };
 const observador = new IntersectionObserver(call,options);
-
-$observador.forEach(element =>{
+$observador.forEach(element => {
     observador.observe(element);
 });
-
 function call(entries){
     entries.forEach(element => {
-        let v = element;
-        if(element.isIntersecting){
-            $titulo[0].style.opacity="1";
-            $contenidoTexo[0].style.opacity="1"
-            $yo.style.opacity="1";
-            $yo.style.transform="translate(10px,0px)"
-            $yo.style.transform="scale(1.3,1.3)"
-        }
-        if(v.target == $habilidades){
-            if(v.isIntersecting){
-                $habilidades.style.opacity="1"
-                $titulo[1].style.opacity="1" 
+        if(element.target == $acerca){
+            if(element.isIntersecting){
+                $acerca.style.opacity="1";
             }
-        }
-        if(v.target == $empleos){
-            if(v.isIntersecting){
-                $empleos.style.opacity="1"
-                $titulo[1].style.opacity="1";
-                $titulo[2].style.opacity="1";
-                $titulo[3].style.opacity="1";
-                $titulo[4].style.opacity="1";
-                $contenidoTexo[1].style.opacity="1"
-                $contenidoTexo[2].style.opacity="1"
-            }
-        }
+        }       
     });
+    
 }
+
+$intereses.forEach(element => {
+    element.addEventListener("mouseover",e=>{
+        element.style.opacity="1"
+        element.style.background="rgb(1, 111, 201,.5)"
+    })
+});
+
+$intereses.forEach(element => {
+    element.addEventListener("mouseout",e=>{
+        element.style.opacity="0"
+    })
+});
+
