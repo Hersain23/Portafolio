404d7ee8a8748808a255959d59ee3c8bd0804785
unknown <dguarnerosmarquez@gmail.com>
2021-03-18 20:57:06 -0600

    Portafolio 4.0

--- JS/Animaciones.js
+++ JS/Animaciones.js
@@ -1,6 +1,8 @@
 const $observador = document.querySelectorAll(".ob");
 const $acerca = document.getElementById("acerca");
-const $intereses = document.querySelectorAll(".container-interes-contenido");
+const $hobby = document.getElementById("hobby");
+const $contacto = document.getElementById("contacto");
+const $r = document.getElementById("containerRedes");
 const $up = document.querySelector(".icon-circle-up");
 let mediaQuery = window.matchMedia("(max-width:720px)")
 const options = {
@@ -15,23 +17,99 @@ function call(entries){
     entries.forEach(element => {
         if(element.target == $acerca){
             if(element.isIntersecting){
+                let xhr = new XMLHttpRequest();
+                xhr.addEventListener("readystatechange",(e)=>{
+                    if(xhr.readyState != 4)return;
+                    if(xhr.status >=200 && xhr.status<300){
+                        let res = xhr.responseText;
+                        $acerca.innerHTML = res;
+                    }
+                    else{
+                        let message = xhr.statusText||"Ocurrio un error";
+                        alert(message);
+                    }
+                })
+                xhr.open("GET","acerca.html");
+                xhr.setRequestHeader("Content-type","text/html;charset=utf-8");
+                xhr.send();
                 $acerca.style.opacity="1";
                 $up.style.opacity="1";
             }
-        }       
-    });
-    
+        }   
+        if(element.target == $hobby){
+            if(element.isIntersecting){
+                let xhr = new XMLHttpRequest();
+                xhr.addEventListener("readystatechange",(e)=>{
+                    if(xhr.readyState !=4)return;
+                    if(xhr.status >=200 && xhr.status<300){
+                        let res = xhr.responseText;
+                        $hobby.innerHTML = res;
+                        const $intereses = document.querySelectorAll(".container-interes-contenido");
+                        int($intereses);
+                    }   
+                    else{
+                        let message = xhr.statusText || "Ocurrio un error";
+                        alert(message);
+                    }
+                })
+                xhr.open("GET","hobby.html");
+                xhr.setRequestHeader("Content-type","text/html;charset=utf-8");
+                xhr.send();
+            }
+        }    
+        if(element.target == $contacto){
+            if(element.isIntersecting){
+                let xhr = new XMLHttpRequest();
+                xhr.addEventListener("readystatechange",(e)=>{
+                    if(xhr.readyState != 4)return;
+                    if(xhr.status >= 200 && xhr.status <300){
+                        let res = xhr.responseText;
+                        $contacto.innerHTML = res;
+                    }
+                    else{
+                        let message = xhr.statusText || "Ocurrio un erro";
+                        alert(message);
+                    }
+                })
+                xhr.open("GET","contacto.html");
+                xhr.setRequestHeader("Content-type","text/html;charset=utf-8");
+                xhr.send();
+            }
+        }
+        if(element.target == $r){
+            if(element.isIntersecting){
+                let xhr = new XMLHttpRequest();
+                xhr.addEventListener("readystatechange",(e)=>{
+                    if(xhr.readyState !=4)return;
+                    if(xhr.status >= 200 && xhr.status < 300){
+                        let res = xhr.responseText;
+                        $r.innerHTML = res;
+                    }
+                    else{
+                        let message = xhr.statusText || "Ocurrio un error";
+                        alert(message);
+                    }
+                })
+                xhr.open("GET","redes.html");
+                xhr.setRequestHeader("Content-type","text/html;charset=utf-8");
+                xhr.send();
+            }
+        }
+    }); 
 }
+
+function int($intereses){
 $intereses.forEach(element => {
     element.addEventListener("mouseover",e=>{
-        element.style.opacity="1"
-        element.style.background="rgb(1, 111, 201,.5)"
+        element.style.opacity="1";
+        element.style.background="rgb(1, 111, 201,.5)";
     })
 });
 
 $intereses.forEach(element => {
     element.addEventListener("mouseout",e=>{
-        element.style.opacity="0"
+        element.style.opacity="0";
     })
 });
+}
 
